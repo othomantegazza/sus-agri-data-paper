b908b4bce3ba39e14543eda18acb60b3528d94d5
Otho Mantegazza <otho.mantegazza@gmail.com>
2023-10-09 09:39:40 +0200

    move graphical setup to a separate script

---
+++ R/graph-setup.R
@@ -0,0 +1,58 @@
+library(dplyr)
+library(tibble)
+library(tidyr)
+library(ggplot2)
+library(forcats)
+library(stringr)
+library(readxl)
+library(readr)
+library(janitor)
+library(glue)
+library(purrr)
+library(jsonlite)
+library(skimr)
+library(paletteer)
+library(grid)
+library(gtable)
+library(gridtext)
+
+# GRAPH SETUP ---------------------------------------------------
+
+unit_type <- 'mm'
+line_width <- 1.2
+width <- 150
+height <- width
+base_size <- 10
+categorical_palette <- paletteer_d("futurevisions::atomic_blue")
+colours_dia <- categorical_palette[2:3]
+# colour_base <- categorical_palette[2]
+colour_base <- '#3982a4'
+size_scale <- 2.8455
+text_size_plot <- base_size/size_scale
+
+theme_set(
+  theme_minimal(
+    base_size = base_size,
+  ) +
+    theme(
+      plot.background = element_rect(fill = 'white',
+                                     colour = 'white'),
+      plot.margin = margin(20,5,5,5),
+      axis.text.x = element_text(hjust = .5,
+                                 vjust = .5,
+                                 colour = "black",
+                                 size = base_size),
+      axis.text.y = element_text(vjust = 0.5,
+                                 colour = "black",
+                                 size = base_size),
+      axis.ticks = element_line(),
+      axis.line = element_line(linewidth = line_width),
+      legend.position = "bottom",
+      panel.grid = element_line(
+        colour = 'black',
+        size = line_width*.1,
+        linetype = '11'
+      ),
+      panel.grid.minor = element_blank()
+    )
+)
