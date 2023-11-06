3873e8410d0e0c781b2e731549430c36b280c520
Otho Mantegazza <otho.mantegazza@gmail.com>
2023-11-06 11:22:34 +0100

    compare dois in various files

--- 400-compare-selected.R
+++ 400-compare-selected.R
@@ -29,6 +29,11 @@ synthesis <-
   select(doi, fpid) %>% 
   distinct()
 
+pico <- 
+  read_csv(
+    "data/output/9_pico_cat_result.csv"
+  )
+
 # select ma selected without blue status ------------------------
 
 screening_blue <- 
@@ -36,13 +41,41 @@ screening_blue <-
   select(-status_details) %>% 
   filter(status == "B")
 
-synthesis %>% 
+missing_screening <- 
+  synthesis %>% 
   left_join(
     screening_blue
   ) %>% 
-  filter(is.na(status))
+  filter(is.na(status)) %>% 
+  select(-status, -year)
 
-screening_blue %>% 
+missing_screening %>% 
+  write_csv(
+    "data/output/missing-in-screening.csv"
+  )
+
+missing_impacts <- 
+  screening_blue %>% 
   anti_join(
     synthesis
-  ) 
+  )
+
+missing_impacts %>% 
+  write_csv(
+    "data/output/missing-in-impacts.csv"
+  )
+
+# missing g lines -----------------------------------------------
+
+missing_in_synthesis <- 
+  pico %>% 
+  anti_join(
+    synthesis,
+    by = c("doi", "fpid")
+  ) %>% 
+  select("doi", "fpid")
+
+missing_in_synthesis %>% 
+  write_csv(
+    "data/output/missing-G-rows.csv"
+  )
