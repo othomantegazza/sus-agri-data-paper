b7d069b0c170a1d271162b6024b9dddc04b4c27a
Otho Mantegazza <otho.mantegazza@gmail.com>
2023-11-08 21:29:27 +0100

    clean screen tab

--- 090-clean-search.R
+++ 090-clean-search.R
@@ -28,21 +28,52 @@ list.files(
 search_tab <- 
   read_excel(
     'data/MerFPs_Search_20231017.xlsx'
-  ) 
+  ) %>% 
+  clean_names()
+  
 
 search_tab <-
   search_tab %>% 
   mutate(
     across(
-      .cols = c("Date.of.search.WOS",
-                "Date.of.search.SCOPUS"),
+      .cols = c("date_of_search_wos",
+                "date_of_search_scopus"),
       .fns = ~as.numeric(.) %>% 
         as.Date(origin = "1899-12-30")
     )
-  ) %>% 
-  clean_names()
+  ) 
 
-search_tab <-
+search_tab <- 
+  search_tab %>% 
+  select(
+    fpid,
+    x1,
+    nb_papers_scopus,
+    date_of_search_scopus,
+    nb_papers_wos,
+    date_of_search_wos,
+    nb_papers_after_merging
+  ) %>% 
+  summarise(
+    across(
+      .cols = c(
+        nb_papers_scopus,
+        date_of_search_scopus,
+        nb_papers_wos,
+        date_of_search_wos,
+        nb_papers_after_merging
+      ),
+      .fns = ~max(., na.rm = T)
+    ),
+    .by = fpid
+  )
+
+search_tab %>% 
+  write_csv(
+    "data/output/03-search.csv"
+  )
+
+search_dates <-
   search_tab %>% 
   select(fpid, date_of_search_wos, date_of_search_scopus) %>% 
   pivot_longer(cols = !fpid) %>% 
@@ -57,7 +88,7 @@ search_tab <-
   arrange(value) %>% 
   rename(date_of_search = value)
 
-search_tab %>% 
+search_dates %>% 
   write_csv(
-    "data/output/3-search-dates.csv"
+    "data/output/03-search-dates.csv"
   )
