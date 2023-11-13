3b8bb02fad5599cdb1314ef28635d0bfbcd9ca27
Otho Mantegazza <otho.mantegazza@gmail.com>
2023-11-13 22:21:57 +0100

    make functional later

--- R/viz-screening-years.R
+++ R/viz-screening-years.R
@@ -28,178 +28,178 @@
 #   write_csv('data/output/4-systematic-screening-MISSING-DATA.csv')
 
 # screening date ------------------------------------------------
-
-screening_dates <- 
-  read_csv("data/output/03-search-dates.csv") 
-
-screening_dates <- 
-  screening_dates %>% 
-  mutate(date_of_search = date_of_search %>% {
-    case_when(
-      is.infinite(.) ~ NA,
-      TRUE ~ .
-    )
-  }) %>% 
-  arrange(date_of_search) %>% 
-  mutate(part = rep(1:2, each = n()/2)) %>% 
-  nest(.by = part)
-
-systematic_screening_clean <- 
-  systematic_screening_clean %>% 
-  mutate(
-    Year = Year %>% {
-      case_when(. <= 2004 ~ "< 2004",
-                TRUE ~ as.character(.))
-    }
-  ) %>% 
-  mutate(Year = as.factor(Year))
-
-# viz -----------------------------------------------------------
-
-plot_screening_year <- function(part) {
-  p <-
-    systematic_screening_clean %>% 
-    filter(
-      FPID %in% screening_dates$data[[part]]$fpid
-    ) %>% 
-    left_join(
-      screening_dates$data[[part]],
-      by = c("FPID" = "fpid")
-    ) %>% 
-    mutate(
-      FPID = paste(
-        date_of_search, 
-        FPID %>% str_wrap(width = 20),
-        sep = "\n"
-      )
-    ) %>% 
-    count(FPID, Year) %>%
-    complete(FPID, Year, fill = list(n = 0)) %>% 
-    ggplot() +
-    aes(x = Year,
-        y = n) +
-    geom_col(
-      fill = 'black',
-      colour = 'black',
-      size = line_width/2,
-      width = 1
-    ) +
-    geom_hline(yintercept = 0, 
-               # linewidth = 0,
-               colour = "white") +
-    facet_wrap(
-      facets = "FPID",
-      ncol = 1, 
-      strip.position = "left"
-      # labeller = label_wrap_gen(width = 15)
-    ) +
-    labs(
-      x = NULL,
-      y = NULL
-    ) +
-    scale_y_continuous(position = "right",
-                       breaks = c(0, 50, 100), 
-                       limits = c(0, NA)) +
-    theme(
-      axis.line.y = element_blank(),
-      # panel.grid.major.x = element_blank(),
-      axis.line.x = element_blank(),
-      axis.ticks.y = element_blank(),
-      axis.text.x = element_text(
-        angle = 90,
-        hjust = 1,
-        vjust = .5
-      ),
-      axis.text.y = element_text(
-        vjust = 0
-      ),
-      panel.spacing = unit(0, 'mm'),
-      strip.text.y.left = element_text(
-        angle = 0,
-        hjust = 1,
-        vjust = 0
-      )
-    ) 
-  return(p)
-}
-
-p1 <- plot_screening_year(1) %>% ggplotGrob()
-p2 <- plot_screening_year(2) %>% ggplotGrob()
-
-p <- 
-  gtable() %>% 
-  gtable_add_rows(heights = unit(25, "cm")) %>% 
-  gtable_add_cols(widths = unit(c(10, 10), "cm")) %>% 
-  gtable_add_grob(grobs = p1, l = 1, t = 1) %>%
-  gtable_add_grob(grobs = p2, l = 2, t = 1)
-
-p %>% gtable_show_layout()
-
-grid.newpage();grid.draw(p)
-
-
-# 2nd version ---------------------------------------------------
-
-systematic_screening_clean %>% 
-  left_join(
-    screening_dates %>% unnest(cols = c(data)),
-    by = c("FPID" = "fpid")
-  ) %>% 
-  count(FPID, date_of_search, Year) %>% 
-  arrange(desc(date_of_search)) %>% 
-  mutate(
-    FPID = FPID %>% 
-      paste(date_of_search, sep = " - ") %>%
-      as_factor()
-  ) %>% 
-  ggplot() +
-  aes(x = Year,
-      y = FPID,
-      fill = n) +
-  geom_tile() +
-  geom_text(
-    aes(label = n,
-        colour = case_when(
-          n > 30 ~ 'white',
-          TRUE ~ 'black'
-        )
-    ),
-    size = text_size_plot,
-    show.legend = FALSE
-  ) +
-  labs(
-    x = NULL,
-    y = NULL
-  ) +
-  guides(
-    fill = 'none'
-  ) +
-  scale_x_discrete(
-    position = 'top'
-  ) +
-  scale_fill_viridis_c(
-    direction = -1,
-    option = 'G',
-    na.value = '#00000000',
-  ) +
-  scale_colour_manual(
-    values = c('white',
-               'black') %>% 
-      set_names()
-  ) +
-  theme(
-    axis.line = element_blank(),
-    axis.text.x.top = element_text(
-      angle = 90,
-      hjust = 0, 
-      vjust = .5
-    ),
-    axis.ticks = element_line(
-      size = line_width*.2
-    ),
-    panel.grid = element_line(
-      colour = 'black',
-      size = line_width*.1,
-      linetype = '11'
-    )
-  )
+# 
+# screening_dates <- 
+#   read_csv("data/output/03-search-dates.csv") 
+# 
+# screening_dates <- 
+#   screening_dates %>% 
+#   mutate(date_of_search = date_of_search %>% {
+#     case_when(
+#       is.infinite(.) ~ NA,
+#       TRUE ~ .
+#     )
+#   }) %>% 
+#   arrange(date_of_search) %>% 
+#   mutate(part = rep(1:2, each = n()/2)) %>% 
+#   nest(.by = part)
+# 
+# systematic_screening_clean <- 
+#   systematic_screening_clean %>% 
+#   mutate(
+#     Year = Year %>% {
+#       case_when(. <= 2004 ~ "< 2004",
+#                 TRUE ~ as.character(.))
+#     }
+#   ) %>% 
+#   mutate(Year = as.factor(Year))
+# 
+# # viz -----------------------------------------------------------
+# 
+# plot_screening_year <- function(part) {
+#   p <-
+#     systematic_screening_clean %>% 
+#     filter(
+#       FPID %in% screening_dates$data[[part]]$fpid
+#     ) %>% 
+#     left_join(
+#       screening_dates$data[[part]],
+#       by = c("FPID" = "fpid")
+#     ) %>% 
+#     mutate(
+#       FPID = paste(
+#         date_of_search, 
+#         FPID %>% str_wrap(width = 20),
+#         sep = "\n"
+#       )
+#     ) %>% 
+#     count(FPID, Year) %>%
+#     complete(FPID, Year, fill = list(n = 0)) %>% 
+#     ggplot() +
+#     aes(x = Year,
+#         y = n) +
+#     geom_col(
+#       fill = 'black',
+#       colour = 'black',
+#       size = line_width/2,
+#       width = 1
+#     ) +
+#     geom_hline(yintercept = 0, 
+#                # linewidth = 0,
+#                colour = "white") +
+#     facet_wrap(
+#       facets = "FPID",
+#       ncol = 1, 
+#       strip.position = "left"
+#       # labeller = label_wrap_gen(width = 15)
+#     ) +
+#     labs(
+#       x = NULL,
+#       y = NULL
+#     ) +
+#     scale_y_continuous(position = "right",
+#                        breaks = c(0, 50, 100), 
+#                        limits = c(0, NA)) +
+#     theme(
+#       axis.line.y = element_blank(),
+#       # panel.grid.major.x = element_blank(),
+#       axis.line.x = element_blank(),
+#       axis.ticks.y = element_blank(),
+#       axis.text.x = element_text(
+#         angle = 90,
+#         hjust = 1,
+#         vjust = .5
+#       ),
+#       axis.text.y = element_text(
+#         vjust = 0
+#       ),
+#       panel.spacing = unit(0, 'mm'),
+#       strip.text.y.left = element_text(
+#         angle = 0,
+#         hjust = 1,
+#         vjust = 0
+#       )
+#     ) 
+#   return(p)
+# }
+# 
+# p1 <- plot_screening_year(1) %>% ggplotGrob()
+# p2 <- plot_screening_year(2) %>% ggplotGrob()
+# 
+# p <- 
+#   gtable() %>% 
+#   gtable_add_rows(heights = unit(25, "cm")) %>% 
+#   gtable_add_cols(widths = unit(c(10, 10), "cm")) %>% 
+#   gtable_add_grob(grobs = p1, l = 1, t = 1) %>%
+#   gtable_add_grob(grobs = p2, l = 2, t = 1)
+# 
+# p %>% gtable_show_layout()
+# 
+# grid.newpage();grid.draw(p)
+# 
+# 
+# # 2nd version ---------------------------------------------------
+# 
+# systematic_screening_clean %>% 
+#   left_join(
+#     screening_dates %>% unnest(cols = c(data)),
+#     by = c("FPID" = "fpid")
+#   ) %>% 
+#   count(FPID, date_of_search, Year) %>% 
+#   arrange(desc(date_of_search)) %>% 
+#   mutate(
+#     FPID = FPID %>% 
+#       paste(date_of_search, sep = " - ") %>%
+#       as_factor()
+#   ) %>% 
+#   ggplot() +
+#   aes(x = Year,
+#       y = FPID,
+#       fill = n) +
+#   geom_tile() +
+#   geom_text(
+#     aes(label = n,
+#         colour = case_when(
+#           n > 30 ~ 'white',
+#           TRUE ~ 'black'
+#         )
+#     ),
+#     size = text_size_plot,
+#     show.legend = FALSE
+#   ) +
+#   labs(
+#     x = NULL,
+#     y = NULL
+#   ) +
+#   guides(
+#     fill = 'none'
+#   ) +
+#   scale_x_discrete(
+#     position = 'top'
+#   ) +
+#   scale_fill_viridis_c(
+#     direction = -1,
+#     option = 'G',
+#     na.value = '#00000000',
+#   ) +
+#   scale_colour_manual(
+#     values = c('white',
+#                'black') %>% 
+#       set_names()
+#   ) +
+#   theme(
+#     axis.line = element_blank(),
+#     axis.text.x.top = element_text(
+#       angle = 90,
+#       hjust = 0, 
+#       vjust = .5
+#     ),
+#     axis.ticks = element_line(
+#       size = line_width*.2
+#     ),
+#     panel.grid = element_line(
+#       colour = 'black',
+#       size = line_width*.1,
+#       linetype = '11'
+#     )
+#   )
