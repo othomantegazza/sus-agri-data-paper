5ca9fde5a8c7cf427c77d357c36c055901065055
Otho Mantegazza <otho.mantegazza@gmail.com>
2023-11-15 11:44:09 +0100

    workflow clean

--- WORKFLOW-clean.R
+++ WORKFLOW-clean.R
@@ -20,3 +20,30 @@ list.files(
   full.names = T
 ) %>%
   walk(source)
+
+# clean all data ------------------------------------------------
+
+clean_identifiers(
+  'data/MerFPs_DefinitionFP_20231017.xlsx'
+)
+
+clean_search(
+  'data/MerFPs_Search_20231017.xlsx'
+)
+
+clean_criteria(
+  'data/MerFPs_Criteria_20231017.xlsx'
+)
+
+clean_screening(
+  'data/MerFPs_Screening_20231017.xlsx'
+)
+
+clean_extract_standard_metric(
+  df = 'data/MerFPs_Impacts_20231004_metrics harmonisation.xlsx',
+  sheet = 'harmonisation'
+)
+
+clean_impacts(
+  'data/MerFPs_Impacts_20231017.xlsx'
+)
