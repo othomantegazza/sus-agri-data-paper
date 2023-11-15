4a098ebfb0f13595b7bba7117c569088fb9f4c69
Otho Mantegazza <otho.mantegazza@gmail.com>
2023-11-15 13:44:22 +0100

    improve geo plot

--- R/viz-geo.R
+++ R/viz-geo.R
@@ -1,12 +1,18 @@
-build_p_geo <- function(pico_combinations) {
+build_p_geo <- function(pico_combinations,
+                        x_expansion = .15,
+                        fill_color = "white") {
   
   geo <- 
     pico_combinations %>% 
     distinct(doi, geo_coverage) %>% 
     select(geo_coverage) %>% 
-    mutate(geo_coverage = geo_coverage %>% str_split(pattern = ";")) %>% 
+    mutate(
+      geo_coverage = geo_coverage %>% str_split(pattern = ";")
+    ) %>% 
     unnest(geo_coverage) %>%
-    mutate(geo_coverage = geo_coverage %>% str_trim()) %>% 
+    mutate(
+      geo_coverage = geo_coverage %>% str_trim()
+    ) %>% 
     count(geo_coverage, sort = T)
   
   p <- 
@@ -20,15 +26,17 @@ build_p_geo <- function(pico_combinations) {
     geom_col(width = 1,
              linewidth = line_width/2,
              colour = 'black',
-             fill = "white") +
+             fill = fill_color) +
     geom_text(aes(label = n,
                   x = n + 8),
               hjust = 0,
               size = base_size/size_scale) +
     scale_x_continuous(
-      expand = expansion(mult = c(0, .12)),
+      expand = expansion(mult = c(0, x_expansion)),
       position = "top"
     ) +
+    labs(x = "Papers [n]",
+         y = "Geographical Coverage") +
     theme(
       axis.line.x = element_blank(),
       axis.line.y = element_line(linewidth = line_width),
