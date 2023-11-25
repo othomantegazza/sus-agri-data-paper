c530c4ea2fbbe983f662c4c85e7e2336c085c3e5
Otho Mantegazza <otho.mantegazza@gmail.com>
2023-11-25 18:26:31 +0100

    prepare layout to add table

--- R/viz-pico-overview.R
+++ R/viz-pico-overview.R
@@ -1,6 +1,16 @@
+# add table 
+# add table 
+# add table 
 build_pico_overview <- function(
-    pico_results
+    pico_results,
+    table_col_width = unit(2, "cm"),
+    omargin = unit(.5, "cm"),
+    xlab = "Pairwise Comparisons Extracted [n]",
+    ylab = "Farming Practice",
+    sizelab = "Unique Metrics Extracted [n]"
 ) {
+  # browser()
+  line_width <- line_width / 2
   pairwise_comparisons <- 
     pico_results %>% 
     distinct(
@@ -37,19 +47,57 @@ build_pico_overview <- function(
       shape = 3,
       fill = "#FFFFFF00"
     ) +
-    labs(x = "Pairwise Comparisons Extracted [n]",
-         y = "Farming Practice",
-         size = "Unique Metrics Extracted [n]") +
+    geom_vline(xintercept = 0,
+               linewidth = line_width) +
+    labs(x = xlab,
+         y = ylab,
+         size = sizelab) +
     scale_radius(range = c(0, 15)) +
     scale_x_continuous(
       limits = c(0, NA),
-      expand = expansion(c(0, .1))
+      expand = expansion(c(0, .1)),
+      position = "top"
     ) +
     scale_y_discrete(expand = expansion(.03)) +
     theme(axis.line.x = element_blank(),
-          axis.line.y = element_line(
-            linewidth = line_width/2
-          ))
+          axis.line.y = element_blank())
   
-  return(p)  
+  size_guide <- 
+    p %>% 
+    get_legend()
+  
+  p_grob <- 
+    p %>% 
+    ggplotGrob() %>% 
+    gtable_filter(pattern = "panel|axis|xlab-t")
+  
+  p_grob %>% gtable_show_layout()
+  
+  p_out <- 
+    p_grob %>% 
+    gtable_add_cols(
+      widths = table_col_width,
+      pos = 1
+    ) %>% 
+    gtable_add_cols(
+      widths = omargin,
+      pos = 0
+    ) %>% 
+    gtable_add_cols(
+      widths = omargin,
+      pos = -1
+    ) %>% 
+    gtable_add_rows(
+      heights = omargin,
+      pos = 0
+    ) %>% 
+    gtable_add_rows(
+      heights = omargin,
+      pos = -1
+    )
+  
+  p_out %>% gtable_show_layout()
+  p_out %>% grid.draw()
+  
+  return(p_out)  
 }
