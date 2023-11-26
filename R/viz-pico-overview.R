9db6b660e55d228c2125dfd9850fde3df1c409d3
Otho Mantegazza <otho.mantegazza@gmail.com>
2023-11-26 13:12:07 +0100

    add table

--- R/viz-pico-overview.R
+++ R/viz-pico-overview.R
@@ -3,17 +3,17 @@
 # add table 
 build_pico_overview <- function(
     pico_results,
-    table_col_width = unit(2, "cm"),
+    table_col_width = unit(1, "cm"),
     omargin = unit(.5, "cm"),
     xlab = "Pairwise Comparisons Extracted [n]",
     ylab = "Farming Practice",
     sizelab = "Unique Metrics Extracted [n]"
 ) {
-  browser()
-  
   # Setup ---------------------------------------------------------
   
   line_width <- line_width / 2
+  scale_y_expansion <- 
+    scale_y_discrete(expand = expansion(.03))
 
   # Count pairwise comparisons ---------------------------------
 
@@ -37,17 +37,60 @@ build_pico_overview <- function(
     distinct(fpid, metric_std) %>% 
     count(fpid, sort = T) %>% 
     rename(n_metrics = n)
-
-  # Count experiments ---------------------------------------------
   
+  # Put data together ---------------------------------------------
   
-  
-  # Draft main plot -------------------------------------------------
-  
-  p <-
+  to_main_plot <- 
     pairwise_comparisons %>% 
     left_join(metrics) %>% 
-    mutate(fpid = fpid %>% as_factor() %>% fct_rev()) %>% 
+    mutate(fpid = fpid %>% as_factor() %>% fct_rev())
+
+  # Count effects extracted ----------------------------------------
+  
+  effect_extracted <- 
+    pico_results %>%
+    count(fpid) %>%
+    rename(n_effects = n) %>% 
+    mutate(
+      fpid = fpid %>% factor(
+        levels = levels(to_main_plot$fpid)
+      )
+    )
+  
+  # Draft effect Table ---------------------------------------------
+  
+  p_table <-
+    effect_extracted %>% 
+    ggplot() +
+    aes(x = 1,
+        y = fpid) +
+    geom_label(
+      aes(label = paste0(" ", n_effects)),
+      size = text_size_plot,
+      label.size = 0,
+      fill = "white",
+      label.padding = unit(.02, "cm"),
+      hjust = 0,
+      
+    ) +
+    geom_vline(xintercept = 1,
+               linewidth = line_width) +
+    scale_x_continuous(
+      limits = c(1, 1.3),
+      expand = expansion(0)
+    ) +
+    scale_y_expansion +
+    theme(panel.grid.major.x = element_blank()) 
+  
+  p_table <- 
+    p_table %>% 
+    ggplotGrob() %>% 
+    gtable_filter("panel")
+  
+  # Draft main plot ------------------------------------------------
+  
+  p <-
+    to_main_plot %>% 
     ggplot() +
     aes(y = fpid,
         x = n_pairwise_comparisons) +
@@ -72,7 +115,7 @@ build_pico_overview <- function(
       expand = expansion(c(0, .1)),
       position = "top"
     ) +
-    scale_y_discrete(expand = expansion(.03)) +
+    scale_y_expansion +
     theme(axis.line.x = element_blank(),
           axis.line.y = element_blank())
   
@@ -112,6 +155,10 @@ build_pico_overview <- function(
     gtable_add_rows(
       heights = omargin,
       pos = -1
+    ) %>% 
+    gtable_add_grob(
+      grobs = p_table,
+      t = 4, l = 3
     )
   
   p_out %>% gtable_show_layout()
