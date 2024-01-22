c91c647cc5fa83a85fce61b1e5b73cb7210d5b8b
Otho Mantegazza <otho.mantegazza@gmail.com>
2024-01-22 16:03:12 +0100

    classify factors

---
+++ extra.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(readxl)
+
+factors <- read_csv(file = "data/output/imap/09_factors.csv")
+
+factors_andrea <-
+  read_excel(
+  "data/09_factors_Classified.xlsx"
+) %>% 
+  janitor::clean_names() %>% 
+  mutate(factor_nb = factor_nb %>% as.numeric())
+
+factors_andrea <- 
+  factors_andrea %>% 
+  select(doi,
+         fpid,
+         impact_matrix,population_variable_tier1,
+         factor_nb,
+         population_variable_tier2,
+         population_variable_tier3)
+
+factors_out <- 
+  factors %>% 
+  left_join(factors_andrea)
+
+
+factors_out %>% 
+  write_excel_csv2("data/factors-classified.csv")
